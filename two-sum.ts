3308cb9dab206345205d3bac102c51e0823bb79b
Julian M <julianemartinez1995@gmail.com>
2025-10-20 10:13:37 -0400

    Add solution for Two Sum problem

---
+++ two-sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement)!, i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
+
+console.log(twoSum([3, 2, 1, 4], 6));
