39fe47f777673a45bd72f32834a40ff3af8733a5
Julian M <julianemartinez1995@gmail.com>
2025-10-21 18:47:10 -0400

    Add solution for valid-anagram.ts

---
+++ valid-anagram.ts
@@ -0,0 +1,7 @@
+function isAnagram(s: string, t: string): boolean {
+  const sortedS = s.split("").sort().join("");
+  const sortedT = t.split("").sort().join("");
+  return sortedS === sortedT;
+}
+
+console.log(isAnagram("cat", "tac"));
