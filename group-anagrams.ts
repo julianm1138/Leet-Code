fe9effa8f109d1d1e6582e91e8d66f63f0331c0b
Julian M <julianemartinez1995@gmail.com>
2025-10-21 22:31:25 -0400

    Add solution for group-anagrams.ts

---
+++ group-anagrams.ts
@@ -0,0 +1,12 @@
+function groupAnagrams(strs: string[]): string[][] {
+  const anagrams: Record<string, string[]> = {};
+  for (const s of strs) {
+    const key = s.split("").sort().join("");
+    if (!anagrams[key]) {
+      anagrams[key] = [];
+    }
+    anagrams[key].push(s);
+  }
+  return Object.values(anagrams);
+}
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
