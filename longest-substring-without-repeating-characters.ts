154b8bf38b6852213959240451fd4bea16900493
Julian M <julianemartinez1995@gmail.com>
2025-10-23 22:34:13 -0400

    Add longest-substring-without-repeating-characters.ts

--- longest-substring-without-repeating-characters.ts
+++ longest-substring-without-repeating-characters.ts
@@ -1,24 +1,21 @@
 function lengthOfLongestSubstring(s: string): number {
   let start = 0;
-  let end = s.length - 1;
+  let end = 0;
+  let longest = 0;
 
-  while (end >= start) {
-    let substr = s.slice(start, end);
-    const set = new Set(substr);
+  while (end < s.length) {
+    const currentWindow = s.slice(start, end + 1);
+    const set = new Set(currentWindow);
 
-    end--;
-    console.log(substr);
-    if (substr.length === set.size) {
-      return substr.length;
+    if (set.size < currentWindow.length) {
+      start++;
+    } else {
+      end++;
+      longest = Math.max(longest, currentWindow.length);
     }
   }
 
-  return 0;
+  return longest;
 }
-console.log(lengthOfLongestSubstring("bbbbb"));
 
-//longest substring is the whole string
-//set
-//substring
-//compare set to the substring, if substring has different length than set, it means there are duplicates
-//so the longest subtring without duplicates would be the substring with the same length as the set
+console.log(lengthOfLongestSubstring("abcabcbb"));
