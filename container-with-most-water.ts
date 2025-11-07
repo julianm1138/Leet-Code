e1fe7bd3136611b5e048538e9ac9b7ae942aeace
Julian M <julianemartinez1995@gmail.com>
2025-11-06 21:57:03 -0500

    Add container-with-most-water.ts solution

--- container-with-most-water.ts
+++ container-with-most-water.ts
@@ -1,5 +1,26 @@
-function maxArea(height: number[]): number {
-  return 0;
+function maxArea(heights: number[]): number {
+  let left = 0;
+  let right = heights.length - 1;
+
+  let maxArea = 0;
+
+  while (left <= right) {
+    const width = right - left;
+    const height = Math.min(heights[left], heights[right]);
+    const currentArea = width * height;
+    if (heights[left] < heights[right]) {
+      left++;
+    } else {
+      right--;
+    }
+
+    if (currentArea > maxArea) {
+      maxArea = currentArea;
+    }
+    console.log("current", currentArea);
+  }
+
+  return maxArea;
 }
 
-console.log(maxArea([1, 2, 3]));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
