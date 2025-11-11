54e533c102c8c0623cd78faef0f0587f2c8b82ed
Julian M <julianemartinez1995@gmail.com>
2025-11-10 22:16:55 -0500

    Add trapping-rain-water.ts solution

--- trapping-rain-water.ts
+++ trapping-rain-water.ts
@@ -1,3 +1,12 @@
 function trap(height: number[]): number {
+  for (let i = 0; i < height.length; i++) {
+    let maxRight = height[0];
+    let maxLeft = height[0];
+    maxRight = Math.max(height[i], height[i + 1]);
+    maxLeft = Math.max(height[i], height[i - 1]);
+    const waterLevel = Math.min(maxRight, maxLeft);
+  }
+  let trappedWater = waterLevel - height[i];
   return 0;
 }
+console.log(trap([4, 2, 0, 3, 2, 5]));
