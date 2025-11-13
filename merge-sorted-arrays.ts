89de013aa715bfdf0fcf3edb17db2802c2d5f6af
Julian M <julianemartinez1995@gmail.com>
2025-11-13 17:40:21 -0500

    first commit

---
+++ merge-sorted-arrays.ts
@@ -0,0 +1,15 @@
+function mergeSortedArr(
+  arr1: number[],
+  m: number,
+  arr2: number[],
+  n: number
+): number[] {
+  let pointerA = 0;
+  let pointerB = 0;
+
+  //check if pointerB is >= the pointerA's prev, and <= pointerA's next: if yes: assign pointerB's value to arr1's index + 1
+  //if the current element === 0: continue
+  return [];
+}
+
+console.log(mergeSortedArr([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
