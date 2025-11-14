0789faf6ac518cfcc8f79115091d0fc2bbad0185
Julian M <julianemartinez1995@gmail.com>
2025-11-13 20:34:01 -0500

    solution

--- merge-sorted-arrays.ts
+++ merge-sorted-arrays.ts
@@ -4,12 +4,23 @@ function mergeSortedArr(
   arr2: number[],
   n: number
 ): number[] {
-  let pointerA = 0;
-  let pointerB = 0;
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
 
-  //check if pointerB is >= the pointerA's prev, and <= pointerA's next: if yes: assign pointerB's value to arr1's index + 1
-  //if the current element === 0: continue
-  return [];
+  while (j >= 0) {
+    if (i >= 0 && arr1[i] > arr2[j]) {
+      arr1[k] = arr1[i];
+      i--;
+    } else {
+      arr1[k] = arr2[j];
+      j--;
+    }
+
+    k--;
+  }
+
+  return arr1;
 }
 
 console.log(mergeSortedArr([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
