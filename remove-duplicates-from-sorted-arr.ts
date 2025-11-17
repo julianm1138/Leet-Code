923179cac6c60a7a636d2e0ca4685ab727f4e540
Julian M <julianemartinez1995@gmail.com>
2025-11-16 21:27:26 -0500

    second attempt

--- remove-duplicates-from-sorted-arr.ts
+++ remove-duplicates-from-sorted-arr.ts
@@ -1,10 +1,17 @@
-function removeDuplicates(nums: number[]): number {
+function removeDuplicates(nums: any[]): number {
+  let insertPos = 0;
+
   for (let i = 0; i < nums.length; i++) {
-    while (nums[i] === nums[i + 1]) {
-      nums.shift();
-      console.log(nums);
+    if (nums[i] !== "_") {
+      nums[insertPos] = nums[i];
+      insertPos++;
     }
   }
+  for (let i = insertPos; i < nums.length; i++) {
+    nums[i] = "_";
+  }
+
+  console.log("nums", nums);
   return nums.length;
 }
 console.log(removeDuplicates([0, 0, 1, 1, 1]));
